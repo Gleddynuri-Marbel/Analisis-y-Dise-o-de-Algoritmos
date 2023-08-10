5e4e1fd2eee92960ef768399e7288646e320c45e
Gleddynuri-Marbel <86405228+Gleddynuri-Marbel@users.noreply.github.com>
2023-08-10 12:36:13 -0500

    Add files via upload

--- backtracking_mochila/arbolcom_mochila.cpp
+++ backtracking_mochila/arbolcom_mochila.cpp
@@ -52,12 +52,3 @@ int main() {
 
 
 
-/*
- vector<Item> items = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
-    int capacidad = 7;
-
-    mochila mochila(items, capacidad);
-    int maxbeneficio = mochila.resolucion(0, capacidad);
-
-    cout << "Maximum beneficio that can be carried: " << maxbeneficio << endl;
-*/
